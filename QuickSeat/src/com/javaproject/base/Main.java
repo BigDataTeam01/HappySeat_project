e587bcea3d20cf5cd5283e9aaadd6e2b7557fffe
Forre <pulpilisory@gmail.com>
2024-01-06 11:35:27 +0900

    2024.01.06 11:35 페이지 표준 배경 정보 및 글꼴  적용 예시

--- QuickSeat/src/com/javaproject/base/Main.java
+++ QuickSeat/src/com/javaproject/base/Main.java
@@ -4,7 +4,22 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 		//개발 시작
+		
+		
+		
+		/* 
+		 * Description : 
+		 * Author : Forrest D Park
+		 * Date : 2024.01.06 
+		 * 
+		 * Update 2024.1.04  by PDG:
+		 * 			1. 각 페이지 별로 Test code 작성
+		 * 			2.  DB 데이터 준비
+		 * 			3. 피그마 이미지 추출 하여 아이콘 준비
+		 * 
+		 */
 	}
 
 }
--- QuickSeat/src/com/javaproject/page/MovieInformation.java
+++ QuickSeat/src/com/javaproject/page/MovieInformation.java
@@ -2,11 +2,18 @@ package com.javaproject.page;
 
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
+import java.awt.Font; // FONT library
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+import com.javaproject.base.ShareVar;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 
 public class MovieInformation extends JDialog {
 
@@ -14,20 +21,36 @@ public class MovieInformation extends JDialog {
 	private final JPanel contentPanel = new JPanel();
 	
 	/*
-	 * Description : 1.MovieInformation에서 영화선택 버튼을 터치했을시 SelectCinema 화면으로 이동
-	 * 				 2.SelectInformation에서 이전화면 버튼을 터치했을시  SelectMovie 화면으로 이동
-	 * 				 3.SelectInformation에서 첫화면 버튼을 터치했을시  SelectMenu 화면으로 이동
-	 * Date : 2024.01.05 (금요일)
-	 * Author : 박정민,박지환
+	 * Description : 현재 상영중인 영화 목록을 보여주고 고객이 영화를 선택할수 있도록 하는 페이지
 	 * 
+	 * 				 0.이 페이지는 SelectCinema 으로 부터 이동됨.
+	 * 				 1.MovieInformation에서 영화선택 btn -> SelectCinema 화면
+	 * 
+	 * 				 2.SelectInformation에서 이전화면 버튼을 터치했을시  SelectMovie 화면으로 이동?
+	 * 				 3.SelectInformation에서 첫화면 버튼을 터치했을시  SelectMenu 화면으로 이동?
+	 * 
+	 * Date : 2024.01.05 (금요일)
+	 * Author : 박정민,박지환, 박동근
+	 * 
+	 * Update 2024.01.06 by PDG:
+	 * 			1. descripton 수정
+	 * 			2. Test code 작성
+	 * 			3. kiosk set bound sharevar 에서 가져와 지정
+	 * 			4. diaog -> static 
+	 * 			5. 배경 만듬
+	 * 			6. 배민 도현 추가하여 페이지 타이틀 생성
 	 */
 	
 	/**
 	 * Launch the application.
 	 */
+	private static MovieInformation dialog = new MovieInformation();
+
+	
+	
+	
 	public static void main(String[] args) {
 		try {
-			MovieInformation dialog = new MovieInformation();
 			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
 			dialog.setVisible(true);
 		} catch (Exception e) {
@@ -39,27 +62,41 @@ public class MovieInformation extends JDialog {
 	 * Create the dialog.
 	 */
 	public MovieInformation() {
-		setBounds(100, 100, 450, 300);
+		setTitle("영화정보");
+		setBounds(ShareVar.kiosk_loc_x, 
+				  ShareVar.kiosk_loc_y, 
+				  ShareVar.kiosk_width, 
+				  ShareVar.kiosk_hight);
+		
+		
 		getContentPane().setLayout(new BorderLayout());
-		contentPanel.setLayout(new FlowLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
-		{
-			JPanel buttonPane = new JPanel();
-			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
-			getContentPane().add(buttonPane, BorderLayout.SOUTH);
-			{
-				JButton okButton = new JButton("OK");
-				okButton.setActionCommand("OK");
-				buttonPane.add(okButton);
-				getRootPane().setDefaultButton(okButton);
-			}
-			{
-				JButton cancelButton = new JButton("Cancel");
-				cancelButton.setActionCommand("Cancel");
-				buttonPane.add(cancelButton);
-			}
-		}
-	}
+		contentPanel.setLayout(null);
 
-}
+		
+		//	페이지 타이틀 예시 
+		JLabel lbl_pageTitle = new JLabel("영화 선택");
+		lbl_pageTitle.setFont(new Font(ShareVar.kiosk_title_font,
+		 								Font.PLAIN, 
+		 								ShareVar.kiosk_title_font_size));
+		 								
+		lbl_pageTitle.setBounds(ShareVar.kiosk_title_loc_x, 
+								ShareVar.kiosk_title_loc_y, 
+								ShareVar.kiosk_title_width,
+								ShareVar.kiosk_title_hight);
+		
+		contentPanel.add(lbl_pageTitle);
+		
+		
+		
+		
+		
+		JLabel lbl_background = new JLabel("",SwingConstants.CENTER);
+		//lblNewLabel.
+		//Font font = new ("도현체")
+		lbl_background.setIcon(new ImageIcon(MovieInformation.class.getResource("/com/javaproject/image/[QuickSeat]kiosk_background.png")));
+		lbl_background.setBounds(0, 0, 800, 600);
+		contentPanel.add(lbl_background);
+	}
+}// END
