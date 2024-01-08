0e2de2c61ae0a1ebbc93825c1c31f1f1444d5a52
forrestDPark <pulpilisory@gmail.com>
2024-01-08 19:14:26 +0900

    2024.01.08_07:14 버튼 내맘대로 조작하는것 구현!

--- QuickSeat/src/com/javaproject/page/MovieInformation.java
+++ QuickSeat/src/com/javaproject/page/MovieInformation.java
@@ -3,6 +3,10 @@ package com.javaproject.page;
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import java.awt.Font; // FONT library
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Insets;
+import java.awt.RenderingHints;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
@@ -10,12 +14,34 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
 import com.javaproject.base.ShareVar;
+
+
+
 import javax.swing.JLabel;
+import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.geom.RoundRectangle2D;
+import java.awt.Color;
+import java.awt.Component;
+
+import javax.swing.border.Border;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+	
+
+
+// 그냥 스윙에 있는거 다쓰자. 
+
+import java.awt.*;
+
+//swing 에 있는거 다쓴다 
+import javax.swing.*;
 
 
 
@@ -24,7 +50,6 @@ public class MovieInformation extends JDialog{
 
 	private static final long serialVersionUID = 1L;
 	private final JPanel contentPanel = new JPanel();
-	
 	private static final String VIDEO_URL = "https://youtu.be/bLvqoHBptjg?si=nwLN3uBeu65fFt-q"; // 동영상 URL
 	
 	/*
@@ -55,8 +80,80 @@ public class MovieInformation extends JDialog{
 	private static MovieInformation dialog = new MovieInformation();
 	private static SelectCinema SelectCinemadialog = new SelectCinema();
 	private static SelectMovie SelectMoviedialog = new SelectMovie();
+	private JButton btnNewButton;
+	private JButton btnNewButton_1;
+	
 
 	
+	//////////////라운드 버튼박스를 만들기 위한 paintComponent 설정
+
+	class mybutton extends JButton{
+		
+		private Color backgroundColor = new Color(183, 216, 107);
+	    public mybutton(String text, Color bgColor) {
+	        super(text);
+	        this.backgroundColor = bgColor;
+	        init();
+	    }
+	    private void init() {
+	        setContentAreaFilled(false);
+	        setOpaque(false);
+	    }
+
+		
+		
+		
+		@Override
+		public void paintComponent(Graphics g) {
+			
+			int width = getWidth();
+			int height = getHeight();
+			
+			Graphics2D graphics = (Graphics2D) g;
+
+		    graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, 
+		    						  RenderingHints.VALUE_ANTIALIAS_ON);
+
+		    if (getModel().isArmed()) {
+		        graphics.setColor(backgroundColor.darker());
+		    } 
+		    else if (getModel().isRollover()) {
+		        graphics.setColor(backgroundColor.brighter());
+		    } 
+		    else {
+		        graphics.setColor(backgroundColor);
+		    }
+
+		    graphics.fillRoundRect(0, 0, width, height, 10, 10);
+
+		    FontMetrics fontMetrics = graphics.getFontMetrics();
+		    Rectangle stringBounds = fontMetrics.getStringBounds(this.getText(), graphics).getBounds();
+		    int textX = (width - stringBounds.width) / 2;
+		    int textY = (height - stringBounds.height) / 2 + fontMetrics.getAscent();
+		    
+		    
+		    
+		    graphics.setColor(getForeground());
+		    graphics.setFont(getFont());
+		    graphics.drawString(getText(), textX, textY);
+		    graphics.dispose();
+
+		    super.paintComponent(g);
+
+			
+//			Graphics2D button_cust = (Graphics2D) g.create();
+//			button_cust.setColor(backgroundColor);
+//			button_cust.fillRect(0,0,getWidth(), getHeight());
+//			super.paintComponent(button_cust);
+//			button_cust.dispose();
+			
+			
+			//super.paintComponent(g);
+			
+		}
+	}
+	
+	////
 	
 	
 	public static void main(String[] args) {
@@ -77,6 +174,12 @@ public class MovieInformation extends JDialog{
 	/**
 	 * Create the dialog.
 	 */
+	
+	
+	
+	
+	
+	
 	public MovieInformation() {
 		setTitle("영화정보");
 		setBounds(ShareVar.kiosk_loc_x, 
@@ -90,21 +193,6 @@ public class MovieInformation extends JDialog{
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
 		contentPanel.setLayout(null);
 		
-		JLabel lblPoster = new JLabel("");
-		lblPoster.setIcon(new ImageIcon(MovieInformation.class.getResource("/com/javaproject/image/[QuickSeat]포스터_포레스트검프.png")));
-		lblPoster.setBounds(34, 101, ShareVar.poster_width, ShareVar.poster_hight);
-		contentPanel.add(lblPoster);
-		
-		JButton btnNewButton = new JButton("영화선택(임시)");
-		btnNewButton.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				goToSelectCinema();
-			}
-		});
-		btnNewButton.setBounds(498, 317, 161, 51);
-		contentPanel.add(btnNewButton);
-		
 		//이전화면 버튼
 		JLabel lblPageTitle_1 = new JLabel("");
 		lblPageTitle_1.addMouseListener(new MouseAdapter() {
@@ -113,9 +201,11 @@ public class MovieInformation extends JDialog{
 			goToSelectMovie();
 			}
 		});
+		contentPanel.add(getBtnNewButton());
 		lblPageTitle_1.setIcon(new ImageIcon(MovieInformation.class.getResource("/com/javaproject/image/Btn 이전화면.png")));
-		lblPageTitle_1.setBounds(475, 391, 200, 100);
+		lblPageTitle_1.setBounds(551, 54, 200, 100);
 		contentPanel.add(lblPageTitle_1);
+
 
 		
 		//	페이지 타이틀 예시 
@@ -130,6 +220,11 @@ public class MovieInformation extends JDialog{
 								ShareVar.kiosk_title_hight);
 		
 		contentPanel.add(lblPageTitle);
+		
+		JLabel lblPoster = new JLabel("");
+		lblPoster.setIcon(new ImageIcon(MovieInformation.class.getResource("/com/javaproject/image/[QuickSeat]포스터_포레스트검프.png")));
+		lblPoster.setBounds(34, 101, ShareVar.poster_width, ShareVar.poster_hight);
+		contentPanel.add(lblPoster);
 		
 		
 		
@@ -164,10 +259,24 @@ public class MovieInformation extends JDialog{
 		SelectMoviedialog.setVisible(true);
 	}
 	
-	public void goToSelectCinema() {
-		dialog.setVisible(false);
-		dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
-		SelectCinemadialog.setVisible(true);
-	}
 	
+	/// Functions
+	
+	private JButton getBtnNewButton() {
+		if (btnNewButton == null) {
+			btnNewButton = new mybutton("영화 선택",new Color(183, 216, 107));
+			
+			btnNewButton.setFont(new Font("BM Dohyeon", Font.PLAIN, 68));
+
+			btnNewButton.setBounds(436	, 458, 292, 110);
+			//btnNewButton.setOpaque(true);
+		}
+		return btnNewButton;
+		
+		
+		
+
+		
+	}
+
 }// END
